0b1c23178e1c51381e26de202cf0676a06a4a6d0
Cristobal Mejia <cristobal.mejia@gmail.com>
2026-09-23 11:14:12 -0600

    import csv

--- html/default/views/settings/lists/datamanager/import/scripts/ImportCsvFile.groovy
+++ html/default/views/settings/lists/datamanager/import/scripts/ImportCsvFile.groovy
@@ -1,6 +1,10 @@
 import org.entermediadb.asset.importer.BaseImporter
 import org.entermediadb.asset.util.Row
 import org.openedit.Data
+import org.openedit.WebPageRequest
+import org.entermediadb.asset.MediaArchive
+import org.openedit.data.Searcher
+import org.openedit.event.WebEvent
 
 class CsvImporter extends BaseImporter
 {
@@ -15,9 +19,16 @@ class CsvImporter extends BaseImporter
 
 }
 
+WebPageRequest req = context;
+MediaArchive archive = req.getPageValue("mediaarchive");
+
 
 CsvImporter csvimporter = new CsvImporter();
 csvimporter.setModuleManager(moduleManager);
+String searchtype = req.getRequestParameter("searchtype");
+log.info("Importing CSV for search type: " + searchtype);
+Searcher searcher = archive.getSearcherManager().getSearcher(archive.getCatalogId(), searchtype);
+csvimporter.setSearcher(searcher);
 csvimporter.setContext(context);
 csvimporter.setLog(log);
 csvimporter.setMakeId(false);
